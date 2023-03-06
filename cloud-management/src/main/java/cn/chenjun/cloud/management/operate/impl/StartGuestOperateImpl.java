218e62225ed4aed64719e7ed5fec301c0c69236a
chenjun <chenjun@roamblue.cn>
2023-03-06 10:50:11 +0800

    调整启动redis锁

--- cloud-management/src/main/java/cn/chenjun/cloud/management/operate/impl/StartGuestOperateImpl.java
+++ cloud-management/src/main/java/cn/chenjun/cloud/management/operate/impl/StartGuestOperateImpl.java
@@ -39,7 +39,7 @@ public class StartGuestOperateImpl<T extends StartGuestOperate> extends Abstract
         super(tClass);
     }
 
-    @Lock(value = RedisKeyUtil.GLOBAL_LOCK_KEY, write = false)
+    @Lock(value = RedisKeyUtil.GLOBAL_LOCK_KEY)
     @Transactional(rollbackFor = Exception.class)
     @Override
     public void operate(T param) {
