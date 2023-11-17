ad664f1fe0d3991e6f65fb88203b32a05e683f39
chenjun <chenjun@roamblue.cn>
2023-11-17 18:53:52 +0800

    修改光驱bug

--- cloud-agent/src/test/java/cn/chenjun/cloud/test/DomainXmlTest.java
+++ cloud-agent/src/test/java/cn/chenjun/cloud/test/DomainXmlTest.java
@@ -8,7 +8,7 @@ import cn.chenjun.cloud.common.gson.GsonBuilderUtil;
 import cn.chenjun.cloud.common.util.Constant;
 
 public class DomainXmlTest {
-    static String json="{\"name\":\"VM-hNxhKWIIAE\",\"description\":\"TEST\",\"emulator\":\"/usr/libexec/qemu-kvm\",\"osMemory\":{\"memory\":2097152},\"osCpu\":{\"number\":2,\"socket\":0,\"core\":0,\"thread\":0,\"share\":0},\"osCdRoom\":{},\"bus\":\"virtio\",\"osDisks\":[{\"name\":\"VM-hNxhKWIIAE\",\"deviceId\":0,\"volume\":\"/mnt/0981d4d911fa4f1c9f5a4eb396529ff1/3c550ab4ae17485c954c3b9c6f093242\",\"volumeType\":\"qcow2\"}],\"networkInterfaces\":[{\"name\":\"VM-hNxhKWIIAE\",\"driveType\":\"virtio\",\"deviceId\":0,\"mac\":\"00:23:D3:7B:85:35\",\"bridgeName\":\"br0\",\"vlanId\":10}],\"vncPassword\":\"xe5OxJLL\"}";
+    static String json=" {\"name\":\"VM-HNwzXsYg\",\"description\":\"TE\",\"emulator\":\"/usr/libexec/qemu-kvm\",\"osMemory\":{\"memory\":1048576},\"osCpu\":{\"number\":1,\"socket\":0,\"core\":0,\"thread\":0,\"share\":0},\"osCdRoom\":{\"path\":\"/mnt/0981d4d911fa4f1c9f5a4eb396529ff1/f8c5ad53-9d58-4acb-871d-4d506ea8a365\"},\"bus\":\"virtio\",\"osDisks\":[{\"name\":\"VM-HNwzXsYg\",\"deviceId\":0,\"volume\":\"/mnt/0981d4d911fa4f1c9f5a4eb396529ff1/a7f3d381436f422582f327ef75025940\",\"volumeType\":\"qcow2\"}],\"networkInterfaces\":[{\"name\":\"VM-HNwzXsYg\",\"driveType\":\"virtio\",\"deviceId\":0,\"mac\":\"00:23:D3:7B:85:35\",\"bridgeName\":\"br0\",\"vlanId\":10}],\"vncPassword\":\"V5gyV5pL\"}";
     public static void main(String[] args) {
         GuestStartRequest request= GsonBuilderUtil.create().fromJson(json,GuestStartRequest.class);
         System.out.println(DomainXmlUtil.buildDomainXml(NetworkType.OPEN_SWITCH,request));
