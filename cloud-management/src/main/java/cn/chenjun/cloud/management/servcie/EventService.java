a955975b8558f39e89edf5d576e3e64bad9ff796
chenjun <chenjun@roamblue.cn>
2025-02-18 19:14:27 +0800

    代码优化

--- cloud-management/src/main/java/cn/chenjun/cloud/management/servcie/EventService.java
+++ cloud-management/src/main/java/cn/chenjun/cloud/management/servcie/EventService.java
@@ -2,11 +2,13 @@ package cn.chenjun.cloud.management.servcie;
 
 import cn.chenjun.cloud.management.util.RedisKeyUtil;
 import cn.chenjun.cloud.management.websocket.message.NotifyData;
+import cn.hutool.core.thread.ThreadUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author chenjun
@@ -22,6 +24,7 @@ public class EventService {
 
     public <T> void publish(NotifyData<T> notifyData) {
         this.executorService.submit(() -> {
+            ThreadUtil.sleep(1, TimeUnit.SECONDS);
             runner.lockRun(RedisKeyUtil.GLOBAL_RW_LOCK_KET, false, () -> EventService.this.applicationContext.publishEvent(notifyData));
         });
 
